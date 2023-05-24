acac94ded231f6b2373ab8b579b32966956f833f
Cory Huebner <chuebner86@gmail.com>
2023-05-23 22:55:47 -0500

    getting bold with that color scheme

--- budget-ui/app/components/app-menu.tsx
+++ budget-ui/app/components/app-menu.tsx
@@ -1,30 +1,30 @@
-import {
-  AppBar as MuiAppBar,
-  Toolbar,
-  IconButton,
-  Typography,
-  Button,
-} from "@mui/material";
+import { IconButton, Typography, Button, Box } from "@mui/material";
 import { Menu } from "@mui/icons-material";
 
 export function AppMenu() {
   return (
-    <MuiAppBar position="static" sx={{ marginBottom: 2 }}>
-      <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <Menu />
-        </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Budge It
-        </Typography>
-        <Button color="inherit">Login</Button>
-      </Toolbar>
-    </MuiAppBar>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        paddingLeft: 2,
+        paddingRight: 2,
+      }}
+    >
+      <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ mr: 2 }}
+      >
+        <Menu />
+      </IconButton>
+      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        Budge It
+      </Typography>
+      <Button color="inherit">Login</Button>
+    </Box>
   );
 }
--- budget-ui/app/theme.ts
+++ budget-ui/app/theme.ts
@@ -4,6 +4,9 @@ import { red } from "@mui/material/colors";
 // Create a theme instance.
 const theme = createTheme({
   palette: {
+    text: {
+      primary: "#fff",
+    },
     primary: {
       main: "#049628",
     },
@@ -12,6 +15,17 @@ const theme = createTheme({
     },
     error: {
       main: red.A400,
+    },
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          backgroundColor: "rgb(2,137,75)",
+          background:
+            "linear-gradient(115deg, rgba(2,137,75,1) 35%, rgba(8,144,82,1) 100%)",
+        },
+      },
     },
   },
 });
