1656de2febd7d5a36c53c126c38dccfc1c7f223b
Cory Huebner <chuebner86@gmail.com>
2023-09-30 20:28:57 -0500

    working upsert of demo user

--- packages/apps/data-initializer/firebase-users.ts
+++ packages/apps/data-initializer/firebase-users.ts
@@ -5,8 +5,25 @@ import { demoUser } from "./demo-data/users";
 
 const logger = createLogger("firebase-users");
 
+async function findUser(uid: string) {
+  const auth = getFirebaseAdminAuth();
+  try {
+    return await auth.getUser(uid);
+  } catch (err: unknown) {
+    logger.debug({ uid, err }, "Demo user not found");
+    return null;
+  }
+}
+
 export async function addDemoUserToFirebase() {
   const auth = getFirebaseAdminAuth();
-  await auth.createUser(demoUser);
-  logger.info({ userId: demoUser.uid }, "Added demo user to Firebase");
+  const { uid } = demoUser;
+  if (!uid) throw new Error("Expected the demo user to have a specified uid");
+  const existingUser = await findUser(uid);
+  if (existingUser) {
+    await auth.updateUser(uid, demoUser);
+  } else {
+    await auth.createUser(demoUser);
+  }
+  logger.info({ userId: demoUser.uid }, "Ensured demo user is in Firebase");
 }
